1b113a2dcf92a67be1842ac6725c15269447e769
José Paulino <josepaulinog@gmail.com>
2020-04-06 02:16:18 -0400

    Update

--- products/spiders/products.py
+++ products/spiders/products.py
@@ -11,7 +11,7 @@ class BooksSpider(scrapy.Spider):
 
     def parse(self, response):
         for product_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(product_url), callback=self.parse_book_page)
+            yield scrapy.Request(response.urljoin(product_url), callback=self.parse_product_page)
         next_page = response.css("li.next > a ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
