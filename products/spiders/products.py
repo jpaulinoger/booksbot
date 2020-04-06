586efa24b04b49489091a99f19fe84b9ac39a005
José Paulino <josepaulinog@gmail.com>
2020-04-06 01:51:14 -0400

    Update

--- products/spiders/products.py
+++ products/spiders/products.py
@@ -3,20 +3,20 @@ import scrapy
 
 
 class BooksSpider(scrapy.Spider):
-    name = "books"
+    name = "products"
     allowed_domains = ["books.toscrape.com"]
     start_urls = [
         'http://books.toscrape.com/',
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
+        for product_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
+            yield scrapy.Request(response.urljoin(product_url), callback=self.parse_book_page)
         next_page = response.css("li.next > a ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
-    def parse_book_page(self, response):
+    def parse_product_page(self, response):
         item = {}
         product = response.css("div.product_main")
         item["title"] = product.css("h1 ::text").extract_first()
