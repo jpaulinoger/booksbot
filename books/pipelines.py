6c4f25cba3a5914cf3f43c8fdc3c99ab05b849dc
José Paulino <josepaulinog@gmail.com>
2020-04-06 01:42:49 -0400

    Revert "Update"
    This reverts commit aac4cfdce257955ca0b61263ea37f137b739c84e.

--- books/pipelines.py
+++ books/pipelines.py
@@ -6,6 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ProductsPipeline(object):
+class BooksPipeline(object):
     def process_item(self, item, spider):
         return item
--- books/settings.py
+++ books/settings.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-BOT_NAME = 'products'
+BOT_NAME = 'books'
 
-SPIDER_MODULES = ['products.spiders']
-NEWSPIDER_MODULE = 'products.spiders'
+SPIDER_MODULES = ['books.spiders']
+NEWSPIDER_MODULE = 'books.spiders'
 
 ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
