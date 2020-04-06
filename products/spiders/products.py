0281e555f89592f839b97986055ea383e3810cac
José Paulino <josepaulinog@gmail.com>
2020-04-06 02:46:30 -0400

    Update

--- products/spiders/products.py
+++ products/spiders/products.py
@@ -4,27 +4,26 @@ import scrapy
 
 class BooksSpider(scrapy.Spider):
     name = "products"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["sirena.do"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'http://sirena.do/products/category/abarrotes/',
     ]
 
     def parse(self, response):
-        for product_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
+        for product_url in response.css("div.item-card > a.item-title ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(product_url), callback=self.parse_product_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
+        next_page = response.css("ul.pagination > li:last-of-type > a.page-link ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
     def parse_product_page(self, response):
         item = {}
-        product = response.css("div.product_main")
+        product = response.css("div.item-info")
         item["title"] = product.css("h1 ::text").extract_first()
         item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
+            "//div[@class='path-title']/a:last-of-type/preceding-sibling::/a/text()"
         ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
+        item['description'] = product.css("div.has-inform > p ::text").extract_first()
+        item['price'] = product.css("h2 ::text").extract_first()
         yield item
+        
